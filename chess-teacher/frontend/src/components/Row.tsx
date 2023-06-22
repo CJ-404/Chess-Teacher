30225c0cd2098b460102144f5ef211d8533059b2
Charith Jayarangana <cj.ptsz@yahoo.com>
2023-06-22 22:48:40 +0530

    create row component

--- chess-teacher/frontend/src/components/Row.tsx
+++ chess-teacher/frontend/src/components/Row.tsx
@@ -3,3 +3,79 @@
 // row Number
 // 8 X square
 // row Number
+
+import React from "react";
+import Square from "./Square";
+import RowNumber from "./RowNumber";
+
+interface Props {
+    rowNumber: number;
+    player: number;
+    state: any[];
+    onSquareClick: (
+        evt: React.MouseEvent<HTMLDivElement>,
+        row: number,
+        col: number
+    ) => void;
+}
+
+//color
+// 0 = bg-light
+// 1 = bg-success
+
+const Row = ({ rowNumber, player, state, onSquareClick }: Props) => {
+    const generateColors = (): number[] => {
+        let colors: number[] = [];
+        if (player === 0 && rowNumber % 2 === 1) {
+            colors = [0, 1, 0, 1, 0, 1, 0, 1];
+        }
+        if (player === 0 && rowNumber % 2 === 0) {
+            colors = [1, 0, 1, 0, 1, 0, 1, 0];
+        }
+        if (player === 1 && rowNumber % 2 === 1) {
+            colors = [1, 0, 1, 0, 1, 0, 1, 0];
+        }
+        if (player === 1 && rowNumber % 2 === 0) {
+            colors = [0, 1, 0, 1, 0, 1, 0, 1];
+        }
+        return colors;
+    };
+    const getColor = (index: number): string => {
+        if (index == 0) return "bg-light";
+        if (index == 1) return "bg-success";
+        return "bg-danger";
+    };
+
+    const findRowIndex = (): number => {
+        // return player === 1 ? rowNumber - 1 : (8 - rowNumber) % 8;
+        return (8 - rowNumber) % 8;
+    };
+
+    const rowIndex = findRowIndex();
+
+    let colors = generateColors();
+    // colors = toggle === 1 ? colors.reverse() : colors;
+    // console.log(state);
+    return (
+        <>
+            <div className="d-flex justify-content-center align-items-center">
+                <RowNumber digit={rowNumber} />
+                {colors.map((color, index) => {
+                    return (
+                        <Square
+                            onSquareClick={onSquareClick}
+                            color={getColor(color)}
+                            state={state[player === 0 ? index : 7 - index]}
+                            row={rowIndex}
+                            col={player === 0 ? index : 7 - index}
+                            key={index}
+                        />
+                    );
+                })}
+                <RowNumber digit={rowNumber} />
+            </div>
+        </>
+    );
+};
+
+export default Row;
--- chess-teacher/frontend/src/components/RowNumber.tsx
+++ chess-teacher/frontend/src/components/RowNumber.tsx
@@ -1,1 +1,16 @@
 // row number square of a row.
+import React from "react";
+
+interface Props {
+    digit: number;
+}
+
+const RowNumber = ({ digit }: Props) => {
+    return (
+        <div className="wrapper-side text-light d-flex justify-content-center align-items-center">
+            <div>{digit}</div>
+        </div>
+    );
+};
+
+export default RowNumber;
